fd88b6ea4c3101bd9d3690da3bb88ffd6e3969d6
OLD_TS網頁呂正中 <christinlu@igs.com.tw>
2025-01-20 11:59:31 +0800

    add ui with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+const compatConfig = new FlatCompat().config({
+  extends: [
+    'plugin:tailwindcss/recommended',
+    // Other extends...
+  ],
+})
+
+export default withNuxt(
+  antfu({
+    // Enable stylistic formatting rules
+    // stylistic: true,
+
+    // Or customize the stylistic rules
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: 'single', // or 'double'
+    },
+
+    // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+    typescript: true,
+    vue: true,
+
+    ignores: [
+      'scripts/**',
+      '.output/**',
+      // ...globs
+    ],
+  }, ...compatConfig,
+  // 自訂規則
+  {
+    files: ['**/*.vue', '**/*.ts', '**/*.js', '**/*.mjs'],
+    rules: {
+      'no-console': 'off',
+      'unused-imports/no-unused-vars': 'off',
+      'style/max-statements-per-line': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'tailwindcss/no-custom-classname': ['error', {
+        whitelist: ['ui\\-.*', '(bg|text)\\-.*', '.+\\-primary.*', '@.*'],
+      }],
+    },
+  }),
+)
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  compatibilityDate: "2025-01-20"
+})
