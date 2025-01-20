9bc8f1d589f33132e74d7602db32936ba05f245a
OLD_TS網頁呂正中 <christinlu@igs.com.tw>
2025-01-20 13:34:18 +0800

    add tailwind scrollbar plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from 'tailwindcss'
+import scrollBarPlugin from 'tailwind-scrollbar'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 console.log('tailwind config: load themes fonts: ', defaultTheme.fontFamily.sans)
 export default <Partial<Config>>{
+  plugins: [
+    scrollBarPlugin({ nocompatible: true }),
+  ],
   theme: {
     extend: {
       fontFamily: {
